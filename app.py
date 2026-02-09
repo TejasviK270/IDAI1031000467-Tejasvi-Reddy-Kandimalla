ca08a340e6c22313107b8232f8510ce2338e2c0d
TejasviK270 <teju.kandimalla@gmail.com>
2026-02-09 08:53:36 +0530

    Change Google Generative AI API key
    Updated API key for Google Generative AI configuration.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import google.generativeai as genai
 
-genai.configure(api_key="AIzaSyBWWER_Il5ZAwAvnVz4sznagXXVx0xfI7k")
+genai.configure(api_key="AIzaSyBlJ0FSupoYW8NrsxXsZQyDehDLzbwV-N4")
 
 for m in genai.list_models():
     print(m.name, m.supported_generation_methods)
