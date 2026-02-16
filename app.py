e6a2fa26a5e6f721426d431874aabe5584f94106
TejasviK270 <teju.kandimalla@gmail.com>
2026-02-16 11:21:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import io
 
 # 1. Configuration & API Setup
-API_KEY = "AIzaSyAqpQ8oumOJWHuVrtqC3v6iWV-h5Lvc9UM" # 
+API_KEY = "AIzaSyDUy0AyOxa9GWahE_M-yjeeldLkNDBnA2Y" # 
 
 genai.configure(api_key=API_KEY)
 # Using gemini-2.5-flash for high-speed, reliable structured output
